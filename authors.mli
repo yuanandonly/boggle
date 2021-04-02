8d39994db019dbf2e9b192c48267794932e2beda
Your Name <your_netid@cornell.edu>
2021-04-01 23:52:09 -0400

    add authors.ml, authors.mli

---
+++ authors.mli
@@ -0,0 +1,6 @@
+(** CS 3110 Spring 2021 MS1
+
+    @authors Jack Lo (wl428), Emily Hur (esh76), Chris Yuan (cyy6), Richard Lin (rl556) *)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
