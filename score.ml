975924df4b1824a8ff3892660f4218f4410ffa3e
Emily Hur <esh76@cornell.edu>
2021-04-27 17:17:54 -0400

    score mli

--- score.ml
+++ score.ml
@@ -1,16 +1,3 @@
-(* SCORING: 1. single player word hunt testing 2. boggle testing
-   https://www.fgbradleys.com/rules/Boggle.pdf 3. possible scoring on
-   rarity? extra
-   >>https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
-   look at this, and maybe come up with a system to score a word based
-   on the rarities of the word? as in each letter is worth a certain
-   value, with J and Z etc worth more than E. Maybe make the scoring
-   somehow the related to inverse of the letter frequency*)
-
-(* 1. single player word hunt testing, returns int score scoring scheme,
-   based on word length: <3: 0 3: 100 4: 400 5: 800 6+: 400*length -
-   1000 (i.e. 6 is 1400 and add 400 after for each letter)*)
-
 let wordhunt_scoring_helper (acc : int) (word : string) : int =
   acc
   +
@@ -26,17 +13,6 @@ let wordhunt_scoring_single (words : string list) : int =
 let wordhunt_scoring_multi (word_sets : string list list) : int list =
   List.map wordhunt_scoring_single word_sets
 
-(* 2. legal boggle game rules following boggle testing
-   https://www.fgbradleys.com/rules/Boggle.pdf
-
-   takes in a list of each player's word list, return a corresponding
-   list of each player's scores (i.e. player whos list was first in list
-   has score at the front of the int list)
-
-   best way to do this is to go through every word of every player and
-   make a frequency map of every input word. then for each player go
-   thru their word list and if a word has frequency 1 (i.e only that
-   player submitted it), then give them credit. *)
 let boggle_scoring_helper (acc : int) (word : string) : int =
   acc
   +
@@ -50,27 +26,20 @@ let boggle_scoring_helper (acc : int) (word : string) : int =
 
 module StringMap = Map.Make (String)
 
-(** boggle_scoring_single lst is the boggle score for a list `lst` of
-    player words playing on their own *)
 let boggle_scoring_single (words : string list) : int =
   List.fold_left boggle_scoring_helper 0 words
 
-(* freqmap_word_helper adds a word to the frequency map (acc) *)
 let freqmap_word_helper (acc : int StringMap.t) (word : string) :
     int StringMap.t =
   match StringMap.find_opt word acc with
   | None -> StringMap.add word 1 acc
   | Some x -> StringMap.add word (1 + x) acc
 
-(* freqmap_list_helper adds a list of words to the frequency map (acc) *)
 let freqmap_list_helper
     (acc : int StringMap.t)
     (player_word_list : string list) : int StringMap.t =
   List.fold_left freqmap_word_helper acc player_word_list
 
-(** boggle_scoring_multi word_lsts is a list of integers representing
-    the boggle scores corresponding to each player's found words in
-    string list list word_lsts *)
 let boggle_scoring_multi (word_sets : string list list) : int list =
   (* create frequency map*)
   let freqmap =
@@ -93,11 +62,5 @@ let boggle_scoring_multi (word_sets : string list list) : int list =
   (* finally, using comparer to map player word lists to integer scores*)
   List.map comparer word_sets
 
-(* 3. possible scoring on rarity? semioptional
-   https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
-   look at this, and maybe come up with a system to score a word based
-   on the rarities of the word? as in each letter is worth a certain
-   value, with J and Z etc worth more than E. Maybe make the scoring
-   somehow the related to inverse of the letter frequency *)
 let score_rarity (words : string list) : int =
   failwith "Unimplemented, semioptional"
--- score.mli
+++ score.mli
@@ -11,9 +11,11 @@
    based on word length: <3: 0 3: 100 4: 400 5: 800 6+: 400*length -
    1000 (i.e. 6 is 1400 and add 400 after for each letter)*)
 
-   val score_helper: int -> string -> int 
+val wordhunt_scoring_helper : int -> string -> int
 
-   val score_singleplayer: string list -> int 
+val wordhunt_scoring_single : string list -> int
+
+val wordhunt_scoring_multi : string list list -> int list
 
 (* 2. legal boggle game rules following boggle testing
    https://www.fgbradleys.com/rules/Boggle.pdf
@@ -26,20 +28,30 @@
    make a frequency map of every input word. then for each player go
    thru their word list and if a word has frequency 1 (i.e only that
    player submitted it), then give them credit. *)
+val boggle_scoring_helper : int -> string -> int
 
-   (* freqmap_word_helper adds a word to the frequency map (acc) *)
-   val freqmap_word_helper: int StringMap.t -> string
-   
-   (* freqmap_list_helper adds a list of words to the frequency map (acc) *)
-   val freqmap_list_helper: int StringMap.t -> string list -> int StringMap.t 
+module StringMap : Map.S with type key = String.t
 
-   val score_boggle: string list list -> int list 
+(** boggle_scoring_single lst is the boggle score for a list `lst` of
+    player words playing on their own *)
+val boggle_scoring_single : string list -> int
 
-   (* 3. possible scoring on rarity? semioptional
+(* freqmap_word_helper adds a word to the frequency map (acc) *)
+val freqmap_word_helper : int StringMap.t -> string -> int StringMap.t
+
+(* freqmap_list_helper adds a list of words to the frequency map (acc) *)
+val freqmap_list_helper :
+  int StringMap.t -> string list -> int StringMap.t
+
+(** boggle_scoring_multi word_lsts is a list of integers representing
+    the boggle scores corresponding to each player's found words in
+    string list list word_lsts *)
+val boggle_scoring_multi : string list list -> int list
+
+(* 3. possible scoring on rarity? semioptional
    https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
    look at this, and maybe come up with a system to score a word based
    on the rarities of the word? as in each letter is worth a certain
    value, with J and Z etc worth more than E. Maybe make the scoring
    somehow the related to inverse of the letter frequency *)
-   val score_rarity: string list -> int 
-   
+val score_rarity : string list -> int
