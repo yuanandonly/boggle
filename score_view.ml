7fecd4a96ae92216ac6d599b6f9d2e4822a8ea79
Your Name <your_netid@cornell.edu>
2021-05-24 01:17:28 -0400

    finish score_view .ml and .mli

--- score_view.ml
+++ score_view.ml
@@ -1,13 +1,5 @@
-(* prints formatted top 10 scores. the input is a list of tuples where
-   each tuple is contains the player name, boggle score, and wordhunt
-   score.*)
-(* FOR EMILY: just make there be like three columns with lines, format
-   it how you think would look nice basically, can use Board.print_board
-   as inspiration*)
-let print_leaderboard (info : (string * int * int) list) : unit =
-  failwith "unimplemented"
+open Readwrite
 
-(* assumption, size >= length of string*)
 let center (size : int) (s : string) =
   let length = String.length s in
   let s = if length > size then String.sub s 0 size else s in
@@ -20,27 +12,59 @@ let center (size : int) (s : string) =
   in
   String.make lside ' ' ^ s ^ String.make rside ' '
 
+let highscore_helper i e_opt : string =
+  let ten = if i = 10 then "" else " " in
+  let num = ten ^ string_of_int i ^ ". " in
+  match e_opt with
+  | Some e ->
+      let score = e.score |> string_of_int |> center 8 in
+      let name = e.name |> center 8 in
+      let name =
+        if String.length e.name > 8 then name ^ ".." else name ^ "  "
+      in
+      let time = (e.time |> string_of_int) ^ " secs" |> center 9 in
+      num ^ score ^ " " ^ name ^ time
+  | None -> num ^ String.make 28 '-'
+
+let get_entries_subset f gamemode =
+  get_entries gamemode |> List.filter f
+  |> List.sort (fun e1 e2 -> compare e1.score e2.score)
+  |> List.rev
+
 let print_highscores (gamemode : string) : unit =
-  let sorted =
-    Readwrite.get_entries gamemode
-    |> List.filter (fun entry ->
-           if entry.board = 4 then true else false)
-    |> List.sort (fun e1 e2 -> compare e1.score e2.score)
-    |> List.rev
+  let entries =
+    get_entries_subset
+      (fun entry -> if entry.board = 4 then true else false)
+      gamemode
   in
   Printf.printf "------------ TOP 10 ------------";
   Printf.printf "      SCORE    NAME       TIME  ";
-  let makestring i e : string =
-    let ten = if i = 10 then "" else " " in
-    let num = ten ^ string_of_int i ^ ". " in
-    let score = e.score |> string_of_int |> center 8 in
-    let name = e.name |> center 8 in
-    let name =
-      if String.length e.name > 8 then name ^ ".." else name ^ "  "
-    in
-    let time = (e.time |> string_of_int) ^ " secs" |> center 9 in
-    num ^ score ^ " " ^ name ^ time
-  in
   for i = 1 to 10 do
-    Printf.printf (makestring i (List.nth sorted (i - 1)))
+    let e_opt =
+      try Some (List.nth entries (i - 1)) with exn -> None
+    in
+    Printf.printf "%s" (highscore_helper i e_opt)
   done
+
+let player_helper i e : string =
+  let score = e.score |> string_of_int |> center 7 in
+  let board = e.board |> string_of_int |> center 3 in
+  let time = (e.time |> string_of_int) ^ " secs" |> center 9 in
+  " " ^ score ^ " " ^ board ^ " " ^ time
+
+let print_player (gamemode : string) (player : string) =
+  let player_entries =
+    get_entries_subset
+      (fun entry -> if entry.name = player then true else false)
+      gamemode
+  in
+  Printf.printf "%s" (center 28 (player ^ "'s " ^ gamemode ^ " scores"));
+  match List.length player_entries with
+  | 0 ->
+      Printf.printf "%s" (center 28 "No scores found...");
+      Printf.printf "%s" (center 28 "Go play some " ^ gamemode ^ "!")
+  | x ->
+      for i = 1 to x do
+        Printf.printf "%s"
+          (player_helper i (List.nth player_entries (i - 1)))
+      done
--- score_view.mli
+++ score_view.mli
@@ -1,1 +1,11 @@
-val print_leaderboard : (string * int * int) list -> unit
+(** [print_highscores gm] prints the the top 10 highscores played on
+    classic boards (4 x 4) for gamemode [gm] into the terminal.
+
+    - Raises "Not a gamemode" if gm is neither "boggle" or "wordhunt" *)
+val print_highscores : string -> unit
+
+(** [print_player gm pl] prints all the scores for the singleplayer
+    games with gamemode [gm] played by player [pl].
+
+    - Raises "Not a gamemode" if gm is neither "boggle" or "wordhunt" *)
+val print_player : string -> string -> unit
