19e1bfc7533f05269b6e3a7efa0a2f8dd0e31a0f
Your Name <your_netid@cornell.edu>
2021-05-23 20:34:13 -0400

    name changes

--- readwrite.ml
+++ readwrite.ml
@@ -14,8 +14,8 @@ let rec read_helper ic lst =
 
 let read (gamemode : string) : string list =
   match gamemode with
-  | "boggle" -> read_helper (open_in "highscores_boggle.txt") []
-  | "wordhunt" -> read_helper (open_in "highscores_wordhunt.txt") []
+  | "boggle" -> read_helper (open_in "score_entries_boggle.txt") []
+  | "wordhunt" -> read_helper (open_in "score_entries_boggle.txt") []
   | _ -> failwith "Not a gamemode"
 
 let convert_helper (s : string) : entry =
@@ -36,28 +36,10 @@ let write gamemode name score board time : unit =
   let oc =
     match gamemode with
     | "boggle" ->
-        open_out_gen [ Open_append ] 0o666 "highscores_boggle.txt"
+        open_out_gen [ Open_append ] 0o666 "score_entries_boggle.txt"
     | "wordhunt" ->
-        open_out_gen [ Open_append ] 0o666 "highscores_wordhunt.txt"
+        open_out_gen [ Open_append ] 0o666 "score_entries_wordhunt.txt"
     | _ -> failwith "Not a gamemode"
   in
   Printf.fprintf oc "%s %d %d %d" name score board time;
   close_out oc
-
-let print_to_file file score =
-  let oc = open_out_gen [ Open_append; Open_creat ] 0o666 file in
-  Printf.fprintf oc "%d\n" score;
-  close_out oc
-
-let clear_score file ans =
-  if ans = "y" then
-    let oc = open_out_gen [ Open_trunc ] 0o666 file in
-    close_out oc
-  else if ans = "n" then ()
-  else
-    let () =
-      ANSITerminal.(
-        print_string [ magenta; Bold ]
-          "\n\nGame ended due to invalid response.\n\n")
-    in
-    exit 0
