bc53b287625cee2d6a808dbfb5004bc11a6c71c8
Your Name <your_netid@cornell.edu>
2021-05-24 23:30:13 -0400

    small change

--- authors.mli
+++ authors.mli
@@ -1,7 +1,7 @@
 (** CS 3110 Spring 2021 FINAL PROJECT MS3
 
-    @authors Jack Lo (wl428), Emily Hur (esh76), Chris Yuan (cyy6),
-    Richard Lin (rl556) *)
+    Jack Lo (wl428), Emily Hur (esh76), Chris Yuan (cyy6), and Richard
+    Lin (rl556) *)
 
 (** [hours_worked] is the number of hours you worked on this assignment. *)
 val hours_worked : int
